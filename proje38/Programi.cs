6f7647dd51062b0053311348d7f7e87dd86feda3
endercan ergin <87834304+enderergin@users.noreply.github.com>
2023-05-19 15:10:15 +0300

    proje38

---
+++ proje38/Programi.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proje38
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            musteri x = new musteri();
+            x.id = 1;
+            x.isim = "ender";
+            x.soyisim = "ergin";
+            int sonuc = x.yenikayit(x);
+            if ( sonuc >0 )
+            {
+                Console.WriteLine("yeni kayıt başarılı");
+            }
+        }
+    }
+}
---
+++ proje38/musteri.cs
@@ -0,0 +1,41 @@
+﻿    using Microsoft.Win32.SafeHandles;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace proje38
+    {
+        public class musteri
+        {
+            int _id;
+            string _isim;
+            string _soyisim;
+            public string isim { get { return _isim; } set { _isim = value; } }
+
+            public string soyisim { get { return _soyisim; } set { _soyisim = value; } }
+
+            public int id { get { return _id; } set { _id = value; } }
+
+            public int kayitduzenle(musteri m)
+            {
+                Console.WriteLine("kayit düzenle çalıştı");
+                return 1;
+            }
+
+            public int kayitsil(musteri m)
+            {
+                Console.WriteLine("kayit sil çalıştı: {0}");
+                return 1;
+            }
+
+            public int yenikayit(musteri m)
+            {
+                Console.WriteLine("Yeni Kayit Çalıştı");
+                return 1;
+            
+            }
+       
+        }
+    }
